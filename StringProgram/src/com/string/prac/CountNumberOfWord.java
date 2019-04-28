05e0baef13809fc06ddb5a2c217a1c938986abe8
jaiswalparesh <pareshjaiswal22@hotmail.com>
2019-04-28 19:01:16 +0530

    Find number of word in a given string.

---
+++ StringProgram/src/com/string/prac/CountNumberOfWord.java
@@ -0,0 +1,9 @@
+package com.string.prac;
+
+public class CountNumberOfWord {
+	public static void main(String[] args) {
+		String s = "Enter the string";
+		String [] arr  = s.split(" ");
+		System.out.println("Number of word in the string are : " + arr.length);
+	}
+}
