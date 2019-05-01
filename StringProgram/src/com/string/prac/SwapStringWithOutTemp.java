194bc7311c88b57da4f9d4fbbcf0d6ded9822e89
jaiswalparesh <pareshjaiswal22@hotmail.com>
2019-05-01 12:48:14 +0530

    Swap String without using temp.

---
+++ StringProgram/src/com/string/prac/SwapStringWithOutTemp.java
@@ -0,0 +1,13 @@
+package com.string.prac;
+
+public class SwapStringWithOutTemp {
+	public static void main(String[] args) {
+		String s1 = "Java";
+		String s2 = "J2EE";
+
+		s1 = s1 + s2;
+		s2 = s1.substring(0, s1.length() - s2.length());
+		s1 = s1.substring(s2.length());
+		System.out.println(s1 + " " + s2);
+	}
+}
